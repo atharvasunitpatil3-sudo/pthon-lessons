b467d35522deae02ab4c752711ff0e95301b173b
atharvasunitpatil3-sudo <atharvasunitpatil3@gmail.com>
2026-05-22 13:37:44 +0300

    updates

---
+++ lesson2/act2.py
@@ -0,0 +1,11 @@
+x = 5
+name =  "Atharva"
+
+print(x)
+print(name)
+
+firstName = input("Enter your first name :")
+lastName = input("Enter your last name : ")
+
+full_name = firstName + " " + lastName
+print("Fullname is :" , full_name)
